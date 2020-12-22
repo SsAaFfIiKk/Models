508e25c0d152a198470b4d0d1532b424977f231d
SsAaFfIiKk <chf700@yandex.ru>
2020-12-23 02:31:12 +0300

    correct mean

--- Combain.py
+++ Combain.py
@@ -24,6 +24,7 @@ def get_dict(emp_dict, list, fs_state, sd_state):
                        sd_state: sd}
     return emp_dict
 
+
 def get_key(dict):
     keys = []
     for key in dict.keys():
@@ -31,35 +32,29 @@ def get_key(dict):
     return keys
 
 
-def get_mean(list, mn):
-    mean_value = []
-    for i in np.array_split(list, mn):
-        for j in i:
-            mean_value.append(np.mean(j))
-    return mean_value
-
 with open("result.data", "rb") as f:
     results = pickle.load(f)
 
 get_label_eye = {0: "closed", 1: "open"}
 get_label_smile = {0: "poker", 1: "smile"}
 
-eye_dict = {}
-smile_dict = {}
 update_result = {}
-
 interval_key = get_key(results)
 fps = results[interval_key[-1]]
-mn = 3 * fps
+mn = int(3 * fps)
 
 chunk_key = get_key(results[interval_key[0]])
 len_interval_key = len(interval_key) - 1
 
 if __name__ == '__main__':
     for i in range(len_interval_key):
+        eye_entry = {}
+        smile_entry = {}
+        emot_mean = []
         emot = results[interval_key[i]][chunk_key[2]]
-        three_s = get_mean(emot, mn)
-        thirty_s = get_mean(three_s, 30)
+        three_s = chunks(emot, mn)
+        for count in three_s:
+            emot_mean.append(np.mean(count, axis=0))
 
         eye = results[interval_key[i]][chunk_key[0]]
         smile = results[interval_key[i]][chunk_key[1]]
@@ -67,9 +62,13 @@ if __name__ == '__main__':
         eye_predict = get_predict(eye, get_label_eye)
         smile_predict = get_predict(smile, get_label_smile)
 
-        eye_dict = get_dict(eye_dict, eye_predict, "open", "closed")
-        smile_dict = get_dict(smile_dict, smile_predict, "smile", "poker")
+        eye_entry = get_dict(eye_entry, eye_predict, "open", "closed")
+        smile_entry = get_dict(smile_entry, smile_predict, "smile", "poker")
 
-        update_result[interval_key[i]] = {"emotion": three_s,
-                                          "eye": eye_dict,
-                                          "smile": smile_dict }
+        update_result[interval_key[i]] = {"emotion": emot_mean,
+                                          "eye": eye_entry,
+                                          "smile": smile_entry
+                                          }
+# print(update_result["interval_0"]["emotion"][0])
+# print(update_result["interval_1"]["emotion"][0])
+# print(update_result["interval_2"]["emotion"][0])
